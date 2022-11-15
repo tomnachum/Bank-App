2eb538fba3a9cc64e0fb71614cc0e350f10f0d7d
tom-nachum <tom.nachum@gmail.com>
2022-11-15 16:25:18 +0200

    added abstract class db_manager in case db will replace

--- Server/db/db_manager.py
+++ Server/db/db_manager.py
@@ -1,3 +1,18 @@
-class dbManager:
-    def __init__(self, db) -> None:
-        self.db = db
+from abc import ABC, abstractmethod
+from objects.user import User
+from objects.category import Category
+from objects.transaction import Transaction
+
+
+class dbManager(ABC):
+    @abstractmethod
+    def add_user(self, user: User):
+        pass
+
+    @abstractmethod
+    def add_category(self, category: Category):
+        pass
+
+    @abstractmethod
+    def add_transaction(self, transaction: Transaction):
+        pass
