df3eced79c15b9741cca5de265ea81c13c95cf32
tom-nachum <tom.nachum@gmail.com>
2022-11-18 16:50:35 +0200

    created transaction component

--- client/src/components/Transactions/Transaction/Transaction.js
+++ client/src/components/Transactions/Transaction/Transaction.js
@@ -1,6 +1,42 @@
 import React from "react";
 import "./Transaction.css";
+import ListGroup from "react-bootstrap/ListGroup";
+import Badge from "react-bootstrap/Badge";
 
-export default function Transaction() {
-  return <div>Transaction</div>;
+export default function Transaction(props) {
+  function getDate() {
+    const date = props.transaction.date.split(" ")[0];
+    const [year, month, day] = date.split("-");
+    return `${day}/${month}/${year}`;
+  }
+
+  function getHour() {
+    const time = props.transaction.date.split(" ")[1];
+    return time.slice(0, -3);
+  }
+
+  return (
+    <ListGroup.Item
+      as="li"
+      className="d-flex justify-content-between align-items-start"
+    >
+      <div className="container">
+        <div className="ms-2">
+          <div className="fw-bold">{props.transaction.category}</div>
+          {props.transaction.vendor}
+        </div>
+        <div className="ms-2 center-vertically">
+          <div>{getDate()}</div>
+          {getHour()}
+        </div>
+        <Badge
+          bg={props.transaction.amount > 0 ? "success" : "danger"}
+          pill
+          className="center-vertically amount"
+        >
+          {Math.abs(props.transaction.amount)}
+        </Badge>
+      </div>
+    </ListGroup.Item>
+  );
 }
--- client/src/components/Transactions/Transactions.js
+++ client/src/components/Transactions/Transactions.js
@@ -2,26 +2,16 @@ import React from "react";
 import "./Transactions.css";
 import Card from "react-bootstrap/Card";
 import ListGroup from "react-bootstrap/ListGroup";
-import Badge from "react-bootstrap/Badge";
+import Transaction from "./Transaction/Transaction";
 
-export default function Transactions() {
+export default function Transactions(props) {
   return (
     <Card style={{ width: "35rem" }}>
       <Card.Header as="h3">Transactions</Card.Header>
       <ListGroup as="ol">
-        <ListGroup.Item
-          as="li"
-          className="d-flex justify-content-between align-items-start"
-        >
-          <div className="ms-2 me-auto">
-            <div className="fw-bold">Category</div>
-            vendor
-          </div>
-          <div className="ms-2 me-auto center-vertically">date</div>
-          <Badge bg="success" pill className="center-vertically amount">
-            positive amount
-          </Badge>
-        </ListGroup.Item>
+        {props.transactions.map(t => (
+          <Transaction transaction={t}></Transaction>
+        ))}
       </ListGroup>
     </Card>
   );
