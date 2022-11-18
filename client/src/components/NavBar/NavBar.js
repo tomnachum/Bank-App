164bc85091a2585c108ff43f01f25810fb0b601b
tom-nachum <tom.nachum@gmail.com>
2022-11-18 12:22:48 +0200

    added NavBar

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,6 +1,34 @@
 import React from "react";
 import "./NavBar.css";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import { Link } from "react-router-dom";
 
 export default function NavBar() {
-  return <div>NavBar</div>;
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand>Bank App</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to="/transactions">
+              Transactions
+            </Nav.Link>
+            <Nav.Link as={Link} to="/operations">
+              Operations
+            </Nav.Link>
+            <Nav.Link as={Link} to="/breakdown">
+              Breakdown
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+        <Navbar.Collapse className="justify-content-end">
+          <Navbar.Text>Balance:</Navbar.Text>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
